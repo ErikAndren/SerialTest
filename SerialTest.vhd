9c63832bcdc242f4b7c77f21c3e36cad06cdda6b
Erik Zachrisson <erik.andren@gmail.com>
2013-09-29 16:47:42 +0200

    Write Hello

--- SerialTest.vhd
+++ SerialTest.vhd
@@ -1,9 +1,9 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
 
 use work.Types.all;
-use work.txt_util.all;
 
 entity SerialTest is
 	port (
@@ -18,13 +18,17 @@ architecture fpga of SerialTest is
 	signal Cnt_N, Cnt_D : word(9-1 downto 0);
 	
 	signal Char_D, Char_N : word(4-1 downto 0);
-	signal Str_D, Str_N   : word(8*11-1 downto 0);
+	signal Str_D, Str_N   : word(8*5-1 downto 0);
 begin
 	CntSync : process (Clk, Rst_N)
 	begin
 		if (Rst_N = '0') then
 			Cnt_D <= (others => '0');
-			Str_D <= SwapBytes(to_std_logic_vector("Hello World"));
+			Str_D(1*ByteW-1 downto 0*ByteW) <= conv_word(72, ByteW);  -- H
+			Str_D(2*ByteW-1 downto 1*ByteW) <= conv_worD(101, ByteW); -- e
+			Str_D(3*ByteW-1 downto 2*ByteW) <= conv_worD(108, ByteW); -- l
+			Str_D(4*ByteW-1 downto 3*ByteW) <= conv_worD(108, ByteW); -- l
+			Str_D(5*ByteW-1 downto 4*ByteW) <= conv_worD(111, ByteW); -- o
 			Char_D <= (others => '0');
 		elsif rising_edge(Clk) then
 			Cnt_D  <= Cnt_N;
@@ -36,6 +40,9 @@ begin
 	CntAsync : process (Cnt_D, Char_D, Str_D)
 	begin
 		Cnt_N <= Cnt_D + 1;
+		Char_N <= Char_D;
+		Str_N <= Str_D;
+		
 	
 		if (Char_D = 0) then
 			-- Send start bit
