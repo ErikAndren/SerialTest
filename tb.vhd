d0a5ce90688fb6878f7a0eca0bb50e48b9bc9806
Erik Zachrisson <erik@zachrisson.info>
2014-05-17 17:34:19 +0200

    Add various files to project

---
+++ tb.vhd
@@ -0,0 +1,40 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+
+use work.Types.all;
+
+entity tb is
+end entity;
+
+architecture rtl of tb is
+  signal Clk   : bit1;
+  signal Rst_N : bit1;
+  signal SerialOut : bit1;
+begin
+  ClkProc : process
+  begin
+    while true loop
+      Clk <= '0';
+      wait for 20 ns;
+      Clk <= '1';
+      wait for 20 ns;
+    end loop;
+  end process;
+
+  RstProc : Process
+  begin
+    Rst_N <= '0';
+    wait for 300 ns;
+    Rst_N <= '1';
+    wait;
+  end Process;
+
+  DUT : entity work.SerialTestTop
+    port map (
+      AsyncRstN => Rst_N,
+      Clk       => Clk,
+      --
+      SerialOut => SerialOut
+      );
+end architecture;
