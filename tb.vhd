e2432ed9b1c2e42354957673652886e15eab947b
Erik Zachrisson <erik@zachrisson.info>
2014-06-14 22:53:27 +0200

    Add message

--- tb.vhd
+++ tb.vhd
@@ -1,8 +1,10 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
 
 use work.Types.all;
+use work.SerialPack.all;
 
 entity tb is
 end entity;
@@ -16,6 +18,10 @@ architecture rtl of tb is
   signal TestData    : word(8-1 downto 0);
   signal TestDataVal : bit1;
   signal SerialIn    : bit1;
+  signal Busy        : bit1;
+
+  signal DataPtr_N, DataPtr_D : word(4-1 downto 0); 
+  
 begin
   ClkProc : process
   begin
@@ -58,8 +64,67 @@ begin
 
   Baud <= "010";
 
-  TestData <= x"30";
-  TestDataVal <= '1';
+
+  WriteProcSync : process (Clk, Rst_N)
+  begin
+    if Rst_N = '0' then
+      DataPtr_D <= (others => '0');
+    elsif rising_edge(Clk) then
+      DataPtr_D <= DataPtr_N;
+    end if;
+  end process;
+
+  WriteProcAsync : process (DataPtr_D, Busy)
+  begin
+   DataPtr_N <= DataPtr_D;
+   TestDataVal <= '1';
+
+   case DataPtr_D is
+     when "0000" =>
+       TestData <= W;
+
+     when "0001" =>
+       TestData <= Space;
+
+     when "0010" =>
+       TestData <= x"30";
+
+     when "0011" =>
+       TestData <= x"31";
+
+     when "0100" =>
+       TestData <= x"32";
+       
+     when "0101" =>
+       TestData <= x"33";
+
+     when "0110" =>
+       TestData <= Space;       
+
+     when "0111" =>
+       TestData <= x"34";
+
+     when "1000" =>
+       TestData <= x"35";
+
+     when "1001" =>
+       TestData <= x"36";
+
+     when "1010" =>
+       TestData <= x"37";
+       
+     when "1011" =>
+       TestData <= NewLine;
+       
+     when others =>
+       TestData    <= x"FF";
+       TestDataVal <= '0';
+   end case;
+   
+   if Busy <= '0' and DataPtr_D /= x"FF" then
+     DataPtr_N <= DataPtr_D + 1;
+   end if;
+  end process;
   
   TbSerialGen : entity work.SerialGen
     port map (
@@ -71,7 +136,7 @@ begin
       We        => TestDataVal,
       WData     => TestData,
       --
-      SerialOut => SerialIn
-      );
-  
+      SerialOut => SerialIn,
+      Busy      => Busy
+      );  
 end architecture;
